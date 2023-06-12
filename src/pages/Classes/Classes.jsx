c404b18251c1a1e50499077b473e1ee312a656ab
Rafatulislam60 <rafatulislam67@gmail.com>
2023-06-12 21:23:53 +0600

    complete classes section

--- src/pages/Classes/Classes.jsx
+++ src/pages/Classes/Classes.jsx
@@ -13,7 +13,7 @@ const Classes = () => {
   return (
     <div className="w-full  py-24 lg:py-36">
       <Helmet>
-        <title>Fluency Factory | Home</title>
+        <title>Fluency Factory | Classes</title>
       </Helmet>
       <div className="mt-8 overflow-x-auto">
         <table className="table table-zebra w-full">
@@ -31,7 +31,19 @@ const Classes = () => {
             {classes?.map((classData) => (
               <tr key={classData._id}>
                 <td>
-                  <img className="w-10 h-10" src={classData.Image} />
+                  <img className="w-20 h-20" src={classData.Image} />
+                </td>
+                <td>{classData.Name}</td>
+                <td>{classData.Instructor}</td>
+                <td>{classData.Available_seats}</td>
+                <td>{classData.Price}</td>
+                <td>
+                  <button
+                    //   onClick={() => handleAddToCart(item)}
+                    className="btn btn-outline bg-[#dae1f1] text-[#F21855] border-[#202C45] border-0 border-b-4 mt-4"
+                  >
+                    Select
+                  </button>
                 </td>
               </tr>
             ))}
